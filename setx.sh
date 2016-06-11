ff384f9bbd65003602c71d9869ce9ca703776207
Muxaejlb <muxaejlb@gmail.com>
2016-06-11 03:11:18 +0300

    This is the files in my .config/i3

---
+++ setx.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+setxkbmap "us,ru" ",winkeys" "grp:alt_shift_toggle"
+#setxkbmap -layout "us,ru"
+#setxkbmap -option "grp:caps_toggle,grp_led:scroll,compose:ralt"
+#setxkbmap -option "grp:alt_shift_toogle,grp_led:scroll,compose:ralt"
